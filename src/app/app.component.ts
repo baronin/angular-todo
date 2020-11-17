cd186b2317f182b5b6ce1523e5a8358bfc624acd
Yaroslav Baronin <baronin.yaroslav@gmail.com>
2020-11-17 21:39:01 +0200

    completed homework

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
 
+interface Item {
+  purchase: string,
+  done: boolean,
+  price: number,
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+  title:string = 'todo';
+  price:number = 123;
+  text = 'text';
+  items:Item[] = [
+    {purchase: 'Meet', done: true, price: 0.2},
+    {purchase: 'Yellow', done: false, price: 3.2},
+    {purchase: 'Color', done: true, price: 4},
+    {purchase: 'Green', done: false, price: 2},
+  ];
+
+  addItem(purchase:string, price: number) {
+    this.items.push({
+      purchase, done: false, price
+    });
+  };
+
+  removeItem(id: number) {
+   this.items.splice(id, 1);
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    NoopAnimationsModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
